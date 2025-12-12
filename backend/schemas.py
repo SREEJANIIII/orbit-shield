be922e72b3f04d9b3229aee35aa8a90e84a35410
SREEJANII <bhattacharyasreejani@gmail.com>
2025-12-12 18:45:10 +0530

    Add Pydantic models for SpaceObject schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
 
+class SpaceObjectBase(BaseModel):
+    name: str
+    type: str
+    tle_line1: str
+    tle_line2: str
+    size: float
+
+class SpaceObjectCreate(SpaceObjectBase):
+    pass
+
+class SpaceObjectOut(SpaceObjectBase):
+    id: int
+    class Config:
+        orm_mode = True
