27f25c8553aefde0e1996586fadd625c6708ab9d
SREEJANII <bhattacharyasreejani@gmail.com>
2025-12-12 18:41:37 +0530

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -1,1 +1,18 @@
+from sqlalchemy.orm import Session
+import models, schemas
 
+def create_space_object(db: Session, data: schemas.SpaceObjectCreate):
+    obj = models.SpaceObject(
+        name=data.name,
+        type=data.type,
+        tle_line1=data.tle_line1,
+        tle_line2=data.tle_line2,
+        size=data.size
+    )
+    db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
+
+def get_all_objects(db: Session):
+    return db.query(models.SpaceObject).all()
